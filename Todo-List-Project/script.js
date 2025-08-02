b6fdec1695419753a7b6fe789fd00f97a6aaea9a
Fery Gunawan <ferygunawan1101@gmail.com>
2025-08-02 08:23:10 +0700

    finishing todo list project

--- Todo-List-Project/script.js
+++ Todo-List-Project/script.js
@@ -3,6 +3,10 @@ console.log("By Fery Gunawan Sugiarto, 01/08/2025");
 
 let todoList = JSON.parse(localStorage.getItem("todoListSave")) || [];
 
+renderTodoList();
+
+//alert("Apa kamu yakin?");
+
 function clickAdd() {
   const inputElement = document.querySelector(".js-todo-input");
   const dateInputElement = document.querySelector(".js-date-input");
